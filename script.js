5cf787f95fb5593b7ce511e88e195d6320583ec9
jonadevk <mamalidiaw@gmail.com>
2024-10-07 05:38:34 -0600

    agregando fotos y haciendo actualizacion a la app

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const themeToggleBtn = document.getElementById('theme-toggle');
-    const body = document.body;
-  
-    function setTheme(theme) {
-      if (theme === 'dark') {
-        body.classList.add('dark-mode');
-        themeToggleBtn.textContent = '☀️';
-      } else {
-        body.classList.remove('dark-mode');
-        themeToggleBtn.textContent = '🌙';
-      }
-      localStorage.setItem('theme', theme);
+  const themeToggleBtn = document.getElementById('theme-toggle');
+  const body = document.body;
+
+  function setTheme(theme) {
+    if (theme === 'dark') {
+      body.classList.add('dark-mode');
+      themeToggleBtn.textContent = '☀️'; // Cambia el icono a sol
+    } else {
+      body.classList.remove('dark-mode');
+      themeToggleBtn.textContent = '🌙'; // Cambia el icono a luna
     }
-  
-    function toggleTheme() {
-      if (body.classList.contains('dark-mode')) {
-        setTheme('light');
-      } else {
-        setTheme('dark');
-      }
+    localStorage.setItem('theme', theme);
+  }
+
+  function toggleTheme() {
+    if (body.classList.contains('dark-mode')) {
+      setTheme('light');
+    } else {
+      setTheme('dark');
     }
-  
-    // Initialize theme
-    const savedTheme = localStorage.getItem('theme') || 'light';
-    setTheme(savedTheme);
-  
-    // Theme toggle button event listener
-    themeToggleBtn.addEventListener('click', toggleTheme);
-  });
+  }
+
+  // Inicializa el tema
+  const savedTheme = localStorage.getItem('theme') || 'light';
+  setTheme(savedTheme);
+
+  // Escucha el evento del botón de alternancia de tema
+  themeToggleBtn.addEventListener('click', toggleTheme);
+});
